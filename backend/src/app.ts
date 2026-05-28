37e773bc5c394d8f3f722bcc801e75d5b0288098
Uong duc huy <uonghuythcsdg@gmail.com>
2026-05-28 16:24:31 +0700

    .

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ import applicationRoutes from "./routes/application_routes.js";
 const app: Application = express();
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
+app.use(cors());
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
